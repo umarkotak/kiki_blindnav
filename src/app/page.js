735eb240347f30dd4c49dc9375d5724391bedf73
umar <umarkotak@gmail.com>
2023-10-31 14:13:48 +0700

    temp working

--- src/app/page.js
+++ src/app/page.js
@@ -1,52 +1,120 @@
 'use client'
 
-import { useState, useEffect } from 'react'
-import GoogleMapReact from 'google-map-react'
-
-const AnyReactComponent = ({ text }) => <div>{text}</div>
+import { useState, useEffect, useRef } from 'react'
+import { MapContainer, TileLayer, Marker, Popup, useMap, useMapEvents } from 'react-leaflet';
 
 export default function Home() {
-  const [centerMap, setCenterMap] = useState({
-    lat: 10.99835602,
-    lng: 77.01502627
-  })
+  const [clickedPositions, setClickedPositions] = useState([]);
+  const [currentLocation, setCurrentLocation] = useState([-0.11405174352366838, 109.36435626886076]);
 
-  useEffect(()=>{
-    // findMe()
-  }, [])
+  const animateRef = useRef(false)
 
-  function findMe() {
-    var tmpCenterMap = {}
-    navigator.geolocation.getCurrentPosition(({coords: {latitude: lat, longitude: lng}}) => {
-      tmpCenterMap = {
-        lat: lat,
-        lng: lng
+  useEffect(() => {
+    getCurrLOcation()
+  }, []);
+
+  function getCurrLOcation() {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        const { latitude, longitude } = position.coords;
+        setCurrentLocation([latitude, longitude]);
+        console.log("moved", latitude, longitude)
+      },
+      (error) => {
+        console.error('Error getting current location:', error);
       }
-      console.log("from", centerMap, "my position", tmpCenterMap)
-      setCenterMap(tmpCenterMap)
+    );
+  }
+
+  const handleClick = (e) => {
+    const { lat, lng } = e.latlng;
+    console.log(`Latitude: ${lat}, Longitude: ${lng}`);
+    setClickedPositions([...clickedPositions, { lat, lng }]);
+
+    // Send data to your server
+    sendDataToServer(lat, lng, "testaaa");
+  };
+
+  function MyClickEvents() {
+    const map = useMapEvents({
+      click: handleClick,
+    });
+
+    return null;
+  }
+
+  const sendDataToServer = (latitude, longitude, polyline) => {
+    fetch('https://api.shadow-animapu-1.site/server/sri/location/current', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        latitude,
+        longitude,
+        polyline,
+      }),
     })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error('Error:', error));
+  };
+
+  const RecenterAutomatically = (center) => {
+    if (center.length !== 2) { return }
+    const map = useMap();
+     useEffect(() => {
+       map.setView(center);
+     }, center);
+     return null;
+   }
+
+   function SetViewOnClick({ animateRef }) {
+
+    const map = useMapEvents('click', (e) => {
+      map.setView(e.latlng, map.getZoom(), {
+        animate: animateRef.current || false,
+      })
+    })
+
+    console.log("CLICKED")
+    return null
   }
 
   return (
     <main className="flex min-h-screen flex-col">
-      <div className='flex'>
-        <div className='h-[800px] w-3/4'>
-          <GoogleMapReact
-            bootstrapURLKeys={{ key: "" }}
-            center={centerMap}
-            defaultZoom={10}
-            yesIWantToUseGoogleMapApiInternals={true}
-            key={""}
-          >
-            <AnyReactComponent
-              // lat={centerMap.lat}
-              // lng={centerMap.lng}
-              text="My Marker"
+      <div className='flex flex-col'>
+        <div className='h-full w-full ovefrlow-hidden border border-white rounded-lg bg-gray-300'>
+          <MapContainer center={currentLocation} zoom={13} style={{ height: '500px', width: '100%' }}>
+            <TileLayer
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             />
-          </GoogleMapReact>
+            {clickedPositions.map((position, index) => (
+              <Marker key={index} position={position}>
+                <Popup>
+                  A marker at Lat: {position.lat}, Lng: {position.lng}
+                </Popup>
+              </Marker>
+            ))}
+            {/* <MyClickEvents /> */}
+            <RecenterAutomatically center={currentLocation} />
+            <SetViewOnClick animateRef={animateRef} />
+          </MapContainer>
         </div>
-        <div className='p-4'>
-          <button className='p-2 bg-white hover:bg-gray-200 text-black rounded-lg' onClick={()=>findMe()}>Find Me!</button>
+        <div>
+          <button onClick={()=> getCurrLOcation()}>move to current location</button>
+          <p>
+            <label>
+              <input
+                type="checkbox"
+                onChange={() => {
+                  animateRef.current = !animateRef.current
+                }}
+              />
+              Animate panning
+            </label>
+          </p>
         </div>
       </div>
     </main>
